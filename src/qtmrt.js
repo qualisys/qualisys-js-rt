11d2ad5a1c2a5e1c1c9e1b3dc99c5bca80d81135
Reimund Trost <reimund.trost@qualisys.se>
2016-12-11 00:57:26 +0100

    Converted qtmrt object to class.

--- src/qtmrt.js
+++ src/qtmrt.js
@@ -1,102 +1,103 @@
 'use strict';
 
-(function() {
-	var qtmrt = {
-		UINT64_SIZE:              8,
-		UINT32_SIZE:              4,
-		UINT16_SIZE:              2,
-		UINT8_SIZE:               1,
-		FLOAT_SIZE:               4,
+class QtmRt {
+	static get UINT64_SIZE()                      { return 8; }
+	static get UINT32_SIZE()                      { return 4; }
+	static get UINT16_SIZE()                      { return 2; }
+	static get UINT8_SIZE()                       { return 1; }
+	static get FLOAT_SIZE()                       { return 4; }
 
-		ERROR:                    0,
-		COMMAND:                  1,
-		XML:                      2,
-		DATA:                     3,
-		NO_MORE_DATA:             4,
-		C3D_FILE:                 5,
-		EVENT:                    6,
-		DISCOVER:                 7,
-		QTM_FILE:                 8,
-		COMMAND_RESPONSE:         999,
+	static get ERROR()                            { return 0; }
+	static get COMMAND()                          { return 1; }
+	static get XML()                              { return 2; }
+	static get DATA()                             { return 3; }
+	static get NO_MORE_DATA()                     { return 4; }
+	static get C3D_FILE()                         { return 5; }
+	static get EVENT()                            { return 6; }
+	static get DISCOVER()                         { return 7; }
+	static get QTM_FILE()                         { return 8; }
+	static get COMMAND_RESPONSE()                 { return 999; }
 
-		CONNECTED:                1,
-		CONNECTION_CLOSED:        2,
-		CAPTURE_STARTED:          3,
-		CAPTURE_STOPPED:          4,
-		FETCHING_FINISHED:        5,
-		CALIBRATION_STARTED:      6,
-		CALIBRATION_STOPPED:      7,
-		RT_FROM_FILE_STARTED:     8,
-		RT_FROM_FILE_STOPPED:     9,
-		WAITING_FOR_TRIGGER:      10,
-		CAMERA_SETTINGS_CHANGED:  11,
-		QTM_SHUTTING_DOWN:        12,
-		CAPTURE_SAVED:            13,
+	static get CONNECTED()                        { return 1; }
+	static get CONNECTION_CLOSED()                { return 2; }
+	static get CAPTURE_STARTED()                  { return 3; }
+	static get CAPTURE_STOPPED()                  { return 4; }
+	static get FETCHING_FINISHED()                { return 5; }
+	static get CALIBRATION_STARTED()              { return 6; }
+	static get CALIBRATION_STOPPED()              { return 7; }
+	static get RT_FROM_FILE_STARTED()             { return 8; }
+	static get RT_FROM_FILE_STOPPED()             { return 9; }
+	static get WAITING_FOR_TRIGGER()              { return 10; }
+	static get CAMERA_SETTINGS_CHANGED()          { return 11; }
+	static get QTM_SHUTTING_DOWN()                { return 12; }
+	static get CAPTURE_SAVED()                    { return 13; }
 
-		COMPONENT_ALL:                     0,
-		COMPONENT_3D:                      1,
-		COMPONENT_3D_NO_LABELS:            2,
-		COMPONENT_ANALOG:                  3,
-		COMPONENT_FORCE:                   4,
-		COMPONENT_6D:                      5,
-		COMPONENT_6D_EULER:                6,
-		COMPONENT_2D:                      7,
-		COMPONENT_2D_LINEARIZED:           8,
-		COMPONENT_3D_RESIDUALS:            9,
-		COMPONENT_3D_NO_LABELS_RESIDUALS:  10,
-		COMPONENT_6D_RESIDUALS:            11,
-		COMPONENT_6D_EULER_RESIDUALS:      12,
-		COMPONENT_ANALOG_SINGLE:           13,
-		COMPONENT_IMAGE:                   14,
-		COMPONENT_FORCE_SINGLE:            15,
-		COMPONENT_GAZE_VECTOR:             16,
+	static get COMPONENT_ALL()                    { return 0; }
+	static get COMPONENT_3D()                     { return 1; }
+	static get COMPONENT_3D_NO_LABELS()           { return 2; }
+	static get COMPONENT_ANALOG()                 { return 3; }
+	static get COMPONENT_FORCE()                  { return 4; }
+	static get COMPONENT_6D()                     { return 5; }
+	static get COMPONENT_6D_EULER()               { return 6; }
+	static get COMPONENT_2D()                     { return 7; }
+	static get COMPONENT_2D_LINEARIZED()          { return 8; }
+	static get COMPONENT_3D_RESIDUALS()           { return 9; }
+	static get COMPONENT_3D_NO_LABELS_RESIDUALS() { return 10; }
+	static get COMPONENT_6D_RESIDUALS()           { return 11; }
+	static get COMPONENT_6D_EULER_RESIDUALS()     { return 12; }
+	static get COMPONENT_ANALOG_SINGLE()          { return 13; }
+	static get COMPONENT_IMAGE()                  { return 14; }
+	static get COMPONENT_FORCE_SINGLE()           { return 15; }
+	static get COMPONENT_GAZE_VECTOR()            { return 16; }
 
-		LITTLE_ENDIAN: 0,
-		BIG_ENDIAN:    1,
+	static get LITTLE_ENDIAN() { return 0; }
+	static get BIG_ENDIAN() { return 1; }
 
-	};
+	static get HEADER_SIZE() { return 2 * this.UINT32_SIZE }
+	static get byteOrder() { return this.LITTLE_ENDIAN }
 
-	qtmrt.HEADER_SIZE = 2 * qtmrt.UINT32_SIZE;
-	qtmrt.byteOrder   = qtmrt.LITTLE_ENDIAN;
+	static get COMPONENTS() {
+		return {
+			'2D'            : this.COMPONENT_2D,
+			'2DLin'         : this.COMPONENT_2D_LINEARIZED,
+			'3D'            : this.COMPONENT_3D,
+			'3DNoLabels'    : this.COMPONENT_3D_NO_LABELS,
+			'3DRes'         : this.COMPONENT_3D_RESIDUALS,
+			'3DNoLabelsRes' : this.COMPONENT_3D_NO_LABELS_RESIDUALS,
+			'6D'            : this.COMPONENT_6D,
+			'6DEuler'       : this.COMPONENT_6D_EULER,
+			'6DRes'         : this.COMPONENT_6D_RESIDUALS,
+			'6DEulerRes'    : this.COMPONENT_6D_EULER_RESIDUALS,
+			'Image'         : this.COMPONENT_IMAGE,
+			'Analog'        : this.COMPONENT_ANALOG,
+			'AnalogSingle'  : this.COMPONENT_ANALOG_SINGLE,
+			'Force'         : this.COMPONENT_FORCE,
+			'ForceSingle'   : this.COMPONENT_FORCE_SINGLE,
+			'GazeVector'    : this.COMPONENT_GAZE_VECTOR,
+		};
+	}
 
-	qtmrt.COMPONENTS = {
-		'2D'            : qtmrt.COMPONENT_2D,
-		'2DLin'         : qtmrt.COMPONENT_2D_LINEARIZED,
-		'3D'            : qtmrt.COMPONENT_3D,
-		'3DNoLabels'    : qtmrt.COMPONENT_3D_NO_LABELS,
-		'3DRes'         : qtmrt.COMPONENT_3D_RESIDUALS,
-		'3DNoLabelsRes' : qtmrt.COMPONENT_3D_NO_LABELS_RESIDUALS,
-		'6D'            : qtmrt.COMPONENT_6D,
-		'6DEuler'       : qtmrt.COMPONENT_6D_EULER,
-		'6DRes'         : qtmrt.COMPONENT_6D_RESIDUALS,
-		'6DEulerRes'    : qtmrt.COMPONENT_6D_EULER_RESIDUALS,
-		'Image'         : qtmrt.COMPONENT_IMAGE,
-		'Analog'        : qtmrt.COMPONENT_ANALOG,
-		'AnalogSingle'  : qtmrt.COMPONENT_ANALOG_SINGLE,
-		'Force'         : qtmrt.COMPONENT_FORCE,
-		'ForceSingle'   : qtmrt.COMPONENT_FORCE_SINGLE,
-		'GazeVector'    : qtmrt.COMPONENT_GAZE_VECTOR,
-	};
+	constructor() { }
 
-	qtmrt.eventToString = function(eventId) {
+	static eventToString(eventId) {
 		var eventNames = {};
 
-		eventNames[qtmrt.CONNECTED]               = 'Connected';
-		eventNames[qtmrt.CONNECTION_CLOSED]       = 'Connection Closed';
-		eventNames[qtmrt.CAPTURE_STARTED]         = 'Capture Started';
-		eventNames[qtmrt.CAPTURE_STOPPED]         = 'Capture Stopped';
-		eventNames[qtmrt.FETCHING_FINISHED]       = 'Fetching Finished';
-		eventNames[qtmrt.CALIBRATI3ON_STARTED]    = 'Calibration Started';
-		eventNames[qtmrt.CALIBRATION_STOPPED]     = 'Calibration Stopped';
-		eventNames[qtmrt.RT_FROM_FILE_STARTED]    = 'RT From File Started';
-		eventNames[qtmrt.RT_FROM_FILE_STOPPED]    = 'RT From File Stopped';
-		eventNames[qtmrt.WAITING_FOR_TRIGGER]     = 'Waiting For Trigger';
-		eventNames[qtmrt.CAMERA_SETTINGS_CHANGED] = 'Camera Settings Changed';
-		eventNames[qtmrt.QTM_SHUTTING_DOWN]       = 'QTM Shutting Down';
-		eventNames[qtmrt.CAPTURE_SAVED]           = 'Capture Saved';
+		eventNames[this.CONNECTED]               = 'Connected';
+		eventNames[this.CONNECTION_CLOSED]       = 'Connection Closed';
+		eventNames[this.CAPTURE_STARTED]         = 'Capture Started';
+		eventNames[this.CAPTURE_STOPPED]         = 'Capture Stopped';
+		eventNames[this.FETCHING_FINISHED]       = 'Fetching Finished';
+		eventNames[this.CALIBRATI3ON_STARTED]    = 'Calibration Started';
+		eventNames[this.CALIBRATION_STOPPED]     = 'Calibration Stopped';
+		eventNames[this.RT_FROM_FILE_STARTED]    = 'RT From File Started';
+		eventNames[this.RT_FROM_FILE_STOPPED]    = 'RT From File Stopped';
+		eventNames[this.WAITING_FOR_TRIGGER]     = 'Waiting For Trigger';
+		eventNames[this.CAMERA_SETTINGS_CHANGED] = 'Camera Settings Changed';
+		eventNames[this.QTM_SHUTTING_DOWN]       = 'QTM Shutting Down';
+		eventNames[this.CAPTURE_SAVED]           = 'Capture Saved';
 
 		return eventNames[eventId];
-	};
+	}
+}
 
-	module.exports = qtmrt;
-})();
+module.exports = QtmRt;
